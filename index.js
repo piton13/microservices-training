31de571924f95db31eab4aecae0dafc61dd3974a
Igor Mykhaylyuk <igor13luk@gmail.com>
2018-06-09 12:33:06 +0300

    persist data between server reload

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const { send } = require('micro');
 const url = require('url');
-global.visits = {};
+const level = require('level');
+const promisify = require('then-levelup');
+const db = promisify(level('visits.db', {
+	valueEncoding: 'json'
+}));
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 	const { pathname } = url.parse(req.url);
-	if (!global.visits[pathname]) {
-		global.visits[pathname] = 0;
+	let currentVisits = 0;
+	try {
+		currentVisits = await db.get(pathname);
+	} catch(e) {
+		if (e.notFound) { console.log('the key was not found') }
 	}
-	global.visits[pathname] += 1;
+	currentVisits += 1;
+	await db.put(pathname, currentVisits);
 	console.log(pathname);
-	send(res, 200, `The page has ${global.visits[pathname]} visits!`);
+	send(res, 200, `The page has ${currentVisits} visits!`);
 }
